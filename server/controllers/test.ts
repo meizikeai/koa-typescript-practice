e0122419dc7e423824efc076494eb1aca0c2d1c4
qinxikun <qinxikun@blued.com>
2019-10-30 11:14:20 +0800

    update

--- server/controllers/test.ts
+++ server/controllers/test.ts
@@ -21,29 +21,4 @@ export default class Test {
 
     await ctx.render('demo', response)
   }
-
-  public static async getUsers(ctx: BaseContext) {
-    ctx.status = 200
-    ctx.body = { user: 10000 }
-  }
-
-  public static async getUser(ctx: BaseContext) {
-    ctx.status = 200
-    ctx.body = 'The user is Lucky'
-  }
-
-  public static async createUser(ctx: BaseContext) {
-    ctx.status = 200
-    ctx.body = 'The specified e-mail address already exists'
-  }
-
-  public static async updateUser(ctx: BaseContext) {
-    ctx.status = 200
-    ctx.body = 'The user you are trying to update doesn\'t exist in the db'
-  }
-
-  public static async deleteUser(ctx: BaseContext) {
-    ctx.status = 200
-    ctx.body = 'A user can only be deleted by himself'
-  }
 }
