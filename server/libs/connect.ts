97479aa6d46fc5e519ea131094539a5375d79382
qinxikun <qinxikun@blued.com>
2019-11-27 21:45:25 +0800

    update

--- server/libs/connect.ts
+++ server/libs/connect.ts
@@ -1,11 +1,28 @@
+import createMySQLClient from 'ai-mysql-client'
+import createRedisClient from 'ai-Redis-client'
+import { Qconf } from '@blued-core/qconf'
+
 import datum from '../config/datum'
-import { getMysqlPoolByQconfPath as createMySQLClient } from './mysql-client'
-import { getRedisPoolByQconfPath as createRedisClient } from './redis-client'
+import { isLocalPro } from '../config/env'
 
-type configDatum = keyof typeof datum
+const qconf = new Qconf(datum)
+qconf.flag = isLocalPro ? 'production' : ''
 
-const mysqlClient = (conf: configDatum) => createMySQLClient(conf)
-const redisClient = (conf: configDatum) => createRedisClient(conf)
+const mysqlClient = (key: string) => {
+  if (!(key in datum)) {
+    throw new Error(`Can not find the key: [${key}]`)
+  }
+
+  return createMySQLClient({ key, option: qconf })
+}
+
+const redisClient = (key: string) => {
+  if (!(key in datum)) {
+    throw new Error(`Can not find the key: [${key}]`)
+  }
+
+  return createRedisClient({ key, option: qconf })
+}
 
 export {
   mysqlClient,
